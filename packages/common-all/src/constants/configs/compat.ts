a7e03781e9dd673372309884601593b8cb45dc91
sxltd <207480320+sxltd@users.noreply.github.com>
2025-04-27 22:47:51 -0700

    fix compat mapping

--- packages/common-all/src/constants/configs/compat.ts
+++ packages/common-all/src/constants/configs/compat.ts
@@ -8,11 +8,7 @@ export type ConfigMapping = {
 export const CONFIG_TO_MINIMUM_COMPAT_MAPPING: {
   [key: number]: ConfigMapping;
 } = {
-  1: { clientVersion: "0.0.0" },
-  2: { clientVersion: "0.63.0" }, // config consolidation (commands namespace)
-  3: { clientVersion: "0.65.0" }, // config consolidation (workspace namespace)
-  4: { clientVersion: "0.70.0" }, // config consolidation (preview namespace)
-  5: { clientVersion: "0.83.0", softMapping: true }, // config consolidation (publishing namespace): commented this out because adding this compat mapping would prevent users from keeping the v4 config and still use the cli for publishing. re-enable once we remove backward compatibility.
+  5: { clientVersion: "0.1.0", softMapping: true }, // config consolidation (publishing namespace): commented this out because adding this compat mapping would prevent users from keeping the v4 config and still use the cli for publishing. re-enable once we remove backward compatibility.
 };
 
 export class CompatUtils {
