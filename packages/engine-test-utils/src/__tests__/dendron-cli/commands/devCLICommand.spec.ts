261127d7ed2e2a3b2ec28969ce28315ef81f679e
sxltd <207480320+sxltd@users.noreply.github.com>
2025-11-27 11:47:18 -0800

    engine utils builds

--- packages/engine-test-utils/src/__tests__/dendron-cli/commands/devCLICommand.spec.ts
+++ packages/engine-test-utils/src/__tests__/dendron-cli/commands/devCLICommand.spec.ts
@@ -1,19 +1,8 @@
-import { TimeUtils } from "@sxltd/common-all";
 import {
-  tmpDir,
-} from "@sxltd/common-server";
-import {
-  BuildUtils,
   DevCLICommand,
   DevCLICommandOpts,
   DevCommands,
-  PublishEndpoint,
-  SemverVersion,
 } from "@sxltd/dendron-cli";
-import fs from "fs-extra";
-import path from "path";
-import { stub } from "sinon";
-import { runEngineTestV5 } from "../../../engine";
 
 export const runDevCmd = ({
   cmd,
@@ -22,81 +11,3 @@ export const runDevCmd = ({
   const cli = new DevCLICommand();
   return cli.execute({ cmd, ...opts });
 };
-
-describe("build", () => {
-  const cmd = DevCommands.BUILD;
-  // TODO: this test is too brittle. need to revisit later
-  test.skip("ok, build local", async () => {
-    await runEngineTestV5(
-      async () => {
-        // stub lerna.json
-        const root = tmpDir().name;
-        fs.writeJsonSync(path.join(root, "lerna.json"), { version: "1.0.0" });
-        stub(process, "cwd").returns(root);
-
-        const prepPublishLocalStub = stub(
-          BuildUtils,
-          "prepPublishLocal"
-        ).returns(undefined);
-
-        const buildPluginViewsStub = stub(
-          BuildUtils,
-          "buildPluginViews"
-        ).returns();
-        const syncStaticAssetsStub = stub(
-          BuildUtils,
-          "syncStaticAssets"
-        ).returns(Promise.resolve({ staticPath: "" }));
-        const syncStaticAssetsToNextjsTemplateStub = stub(
-          BuildUtils,
-          "syncStaticAssetsToNextjsTemplate"
-        ).returns(Promise.resolve());
-        const installPluginDependenciesStub = stub(
-          BuildUtils,
-          "installPluginDependencies"
-        ).returns({} as any);
-        const compilePluginStub = stub(BuildUtils, "compilePlugin").returns(
-          {} as any
-        );
-        const packagePluginDependenciesStub = stub(
-          BuildUtils,
-          "packagePluginDependencies"
-        ).returns({} as any);
-        const setRegRemoteStub = stub(BuildUtils, "setRegRemote").returns();
-        const restorePluginPkgJsonStub = stub(
-          BuildUtils,
-          "restorePluginPkgJson"
-        ).returns();
-        const sleepStub = stub(TimeUtils, "sleep").returns(Promise.resolve());
-
-        await runDevCmd({
-          cmd,
-          publishEndpoint: PublishEndpoint.LOCAL,
-          upgradeType: SemverVersion.PATCH,
-        });
-        [
-          prepPublishLocalStub,
-          typecheckStub,
-          bumpVersionStub,
-          publishVersionStub,
-          buildPluginViewsStub,
-          syncStaticAssetsStub,
-          syncStaticAssetsToNextjsTemplateStub,
-          installPluginDependenciesStub,
-          compilePluginStub,
-          packagePluginDependenciesStub,
-          setRegRemoteStub,
-          restorePluginPkgJsonStub,
-          sleepStub,
-        ].map((_stub) => {
-          // uncomment to figure out which stub is failing
-          // console.log(_stub);
-          expect(_stub.calledOnce).toBeTruthy();
-        });
-      },
-      {
-        expect,
-      }
-    );
-  });
-});
