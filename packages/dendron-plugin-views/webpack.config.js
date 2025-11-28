57d8a5518699a684ebf1b0c09831a94a97f84564
sxltd <207480320+sxltd@users.noreply.github.com>
2025-11-27 19:50:52 -0800

    webpack before polyfill stuff

---
+++ packages/dendron-plugin-views/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+        // TypeScript/TSX
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        // CSS files
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        },
+        // SCSS modules
+        {
+          test: /\.module\.s[ac]ss$/i,
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                modules: true,
+              },
+            },
+            "sass-loader",
+          ],
+        },
+        // regular SCSS (non-module)
+        {
+          test: /\.s[ac]ss$/i,
+          exclude: /\.module\.s[ac]ss$/i,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+      ],
+  }
+};
